a01439eb5dfce141481d1403dd4ad65cc217bc15
asm-saim <saim777m@gmail.com>
2026-08-09 11:29:10 +0600

    map amd filter method applied

--- problem1.js
+++ problem1.js
@@ -19,4 +19,36 @@ const applyDiscount = (prices, rate) => {
 };
 let prices = [5000, 10000, 2500];
 let rate = 10;
-console.log(applyDiscount(prices,rate));
+console.log(applyDiscount(prices, rate));
+
+/*
+  Problem 2: Passing Students Filter
+  getPassingStudents(students, threshold) — return only the students
+  whose marks are greater than or equal to the given threshold.
+
+  Input:
+    students = [
+      { name: "Rafi", marks: 75 },
+      { name: "Karim", marks: 40 },
+      { name: "Utsho", marks: 60 },
+    ]
+    threshold = 60
+
+  Output:
+    [
+      { name: "Rafi", marks: 75 },
+      { name: "Utsho", marks: 60 },
+    ]
+*/
+const getPassingStudents = (students, threshold) => {
+  const newObj = students.filter((student) => student.marks >= threshold);
+  return newObj;
+};
+let students = [
+  { name: "Rafi", marks: 75 },
+  { name: "Karim", marks: 40 },
+  { name: "Utsho", marks: 60 },
+];
+let threshold = 60;
+
+console.log(getPassingStudents(students, threshold));
